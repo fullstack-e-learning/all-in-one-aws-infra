35243992bfc9a1e0222c5497c740af2a51f39365
amitzrepo <mr.amitkumarpatel@gmail.com>
2024-04-20 13:27:49 +0530

    hello

--- main.tf
+++ main.tf
@@ -219,7 +219,7 @@ resource "aws_lb_listener" "listener_80" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.alb-tg.arn
   }
 }
@@ -232,9 +232,9 @@ resource "aws_lb_target_group_attachment" "my_instance_attachment" {
 }
 
 resource "ansible_host" "host" {
-  count  = local.instance_count
-  name   = aws_instance.foo[count.index].public_ip
-  
+  count = local.instance_count
+  name  = aws_instance.foo[count.index].public_ip
+
   groups = ["ec2"]
   variables = {
     ansible_user                 = "ubuntu"
